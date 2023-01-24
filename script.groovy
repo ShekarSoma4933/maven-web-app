4b61f4d9c20781aefda26e16a3fad2fb53aa711b
ShekarSoma4933 <shekarsoma4933@gmail.com>
2023-01-23 23:24:45 -0500

    changes groovy file

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar(){
 def buildAndPushImage(){
     echo "this is push stage"
     withCredentials([usernamePassword('credentialsId':'nexus-repo-credentials','usernameVariable':'USER','passwordVariable':'PASS')]){
-        sh "docker build -t 143.198.43.144:8083/maven-web-app:2.1"
+        sh "docker build -t 143.198.43.144:8083/maven-web-app:2.1 ."
         sh "echo ${PASS} | docker login -u ${USER} --pasword-std-in 143.198.43.144:8083"
         sh "docker push 143.198.43.144:8083/maven-web-app:2.1"
     }

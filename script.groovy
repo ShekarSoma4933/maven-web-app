6edaaf00b1ab9a1de00cf91c6d5143c72ecc8ab3
ShekarSoma4933 <shekarsoma4933@gmail.com>
2023-01-27 04:31:21 -0500

    dynamic app version

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def buildVersion(){
     echo "this method creates a new version"
     sh 'mvn build-helper:parse-version versions:set \
-     -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions.commit'
+     -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit'
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
     def version = matcher[0][1]
     env.IMAGE_NAME = "$version-$BUILD_NUMBER"

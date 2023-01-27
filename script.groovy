21ee51d408619e251e4c9dd8f6389f3f835cd08f
ShekarSoma4933 <shekarsoma4933@gmail.com>
2023-01-27 04:38:44 -0500

    dynamic app version

--- script.groovy
+++ script.groovy
@@ -12,15 +12,15 @@ def buildVersion(){
 
 def buildJar(){
     echo "this is build stage"
-    sh "mvn package"
+    sh "mvn clean package"
 }
 
 def buildAndPushImage(){
     echo "this is push stage"
     withCredentials([usernamePassword('credentialsId':'nexus-repo-credentials','usernameVariable':'USER','passwordVariable':'PASS')]){
-        sh "docker build -t 143.198.43.144:8083/maven-web-app:$IMAGE_NAME ."
+        sh "docker build -t 143.198.43.144:8083/maven-web-app:${IMAGE_NAME} ."
         sh "echo ${PASS} | docker login -u ${USER} --password-stdin 143.198.43.144:8083"
-        sh "docker push 143.198.43.144:8083/maven-web-app:$IMAGE_NAME"
+        sh "docker push 143.198.43.144:8083/maven-web-app:${IMAGE_NAME}"
     }
 }
 
